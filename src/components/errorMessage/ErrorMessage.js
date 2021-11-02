338d8fa1a02ad3f2f558ecd02f6f655f39b4b171
narf200 <narf200@gmail.com>
2021-11-02 20:22:23 +0500

    feat: add ErrorMessage.js

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './errorMessage.css'
+
+const ErrorMessage = () => {
+    return (
+        <>
+            <img src="https://http.cat/404" alt="404error"/>
+            <span>
+                Something goes wrong
+            </span>
+        </>
+
+);
+};
+
+export default ErrorMessage;
--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService';
-import Spiner from "../spiner";
+import Spiner from "../spiner/";
+import ErrorMessage from '../errorMessage/'
 
 export default class RandomChar extends Component {
 
@@ -15,51 +16,75 @@ export default class RandomChar extends Component {
     state = {
         char: {},
         loading: true,
+        error: false
     }
 
     onCharLoaded = (char) => {
-        this.setState({char})
+        this.setState({
+            char,
+            loading: false
+        })
+    }
+
+    onError = (err) => {
+        this.setState({
+            error: true,
+            loading: false
+        })
     }
 
     updateChar() {
         const id = Math.floor(Math.random()*600 + 25) // ot 25 do 140
         this.gotService.getCharacter(id)
             .then(this.onCharLoaded)
+            .catch(this.onError)
     }
 
     render() {
 
-        const {char: {name, gender, born, died, culture}, loading} = this.state;
+        const {char, loading, error} = this.state;
 
-        if(loading){
-            return <Spiner/>
-        }
+        const errorMessage = error ? <ErrorMessage /> : null;
+        const spinner =  loading ? <Spiner /> : null;
+        const content = !(loading || error) ? <View char={char}/> : null;
 
         return (
             <div className="random-block rounded">
-                <h4>{name}</h4>
-                <ul className="list-group list-group-flush">
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Gender </span>
-                        <span>{gender}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Born </span>
-                        <span>{born}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Died </span>
-                        <span>{died}</span>
-                    </li>
-                    <li className="list-group-item d-flex justify-content-between">
-                        <span className="term">Culture </span>
-                        <span>{culture}</span>
-                    </li>
-                </ul>
+                {errorMessage}
+                {spinner}
+                {content}
             </div>
         );
     }
 }
 
+const View = ({char}) => {
+
+    const {name, gender, born, died, culture} = char;
+
+  return (
+      <>
+          <h4>{name}</h4>
+          <ul className="list-group list-group-flush">
+              <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Gender </span>
+                  <span>{gender}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Born </span>
+                  <span>{born}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Died </span>
+                  <span>{died}</span>
+              </li>
+              <li className="list-group-item d-flex justify-content-between">
+                  <span className="term">Culture </span>
+                  <span>{culture}</span>
+              </li>
+          </ul>
+      </>
+  )
+}
 
 // 20 min
