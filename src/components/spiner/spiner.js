bb63957e4357c57cd198a5b6c7b6c838d5439430
narf200 <narf200@gmail.com>
2021-11-02 19:17:55 +0500

    init: start gotCheck

---
+++ src/components/spiner/spiner.js
@@ -0,0 +1,30 @@
+import React from "react";
+import './spiner.css'
+
+
+const Spiner = () => {
+    return (
+        <div className="loadingio-spinner-spinner-l2gzfz1rsa">
+            <div className="ldio-astoyrks0ka">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    );
+};
+
+export default Spiner;
+
+
+
+
